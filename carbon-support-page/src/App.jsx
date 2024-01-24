c4e361d7ce9ab16e1d354978c7a9a886813fb91a
murasandev <dmurayama93@gmail.com>
2024-01-23 20:28:34 -1000

    move header from sidebar

--- carbon-support-page/src/App.jsx
+++ carbon-support-page/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import { Button, Accordion, AccordionItem } from '@carbon/react'
+import { Button, InlineNotification } from '@carbon/react'
 import Sidebar from './components/Sidebar'
 import './globals.scss'
 import SwitcherImage from './components/SwitcherImage'
@@ -20,8 +20,20 @@ function App() {
         </div>
         <div className="switcher-holder">
           <SwitcherImage />
+          <div className="notification-holder">
+            <InlineNotification
+              aria-label="closes notification"
+              kind="error"
+              onClose={function noRefCheck(){}}
+              onCloseButtonClick={function noRefCheck(){}}
+              statusIconDescription="notification"
+              subtitle="404 Not Found"
+              title="Error:"
+            />
+          </div>
         </div>
       </div>
+      
     </div>
   )
 }
--- carbon-support-page/src/components/Sidebar.jsx
+++ carbon-support-page/src/components/Sidebar.jsx
@@ -1,12 +1,30 @@
 import { Button } from "@carbon/react";
+import './Sidebar.css'
 
 export default function Hello() {
     return(
-        <div>
-            <h1>Support</h1>
-            <Button kind="danger">
-                More...
-            </Button>
+        <div className="sidebar-holder">
+            
+            <div className="button-holder">
+                <Button kind="danger">
+                    Home
+                </Button>
+                <Button kind="danger">
+                    Home
+                </Button>
+                <Button kind="danger">
+                    Home
+                </Button>
+                <Button kind="danger">
+                    Home
+                </Button>
+                <Button kind="danger">
+                    Home
+                </Button>
+                <Button kind="danger">
+                    Home
+                </Button>
+            </div>
         </div>
     )
 }
