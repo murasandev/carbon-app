86da860be8196b6a7cb3ab23735b2e7a42413356
murasandev <dmurayama93@gmail.com>
2024-01-25 19:38:08 -1000

    remove office location from footer component and center social links and other footer links

--- carbon-support-page/src/components/Footer.jsx
+++ carbon-support-page/src/components/Footer.jsx
@@ -1,36 +1,30 @@
-import { Button } from "@carbon/react";
+import { Button, IconButton } from "@carbon/react";
 import "./Footer.css"
+import { LogoFacebook, LogoGithub, LogoInstagram, LogoLinkedin } from '@carbon/react/icons'
 
 export default function Footer() {
     return(
         <div className="footer-holder">
-            {/* careers, about us, contact us, privacy policy,
-            offices, social media links */}
             <div className="footer-links-holder">
                 <Button kind="danger--ghost">Careers</Button>
                 <Button kind="danger--ghost">About Us</Button>
                 <Button kind="danger--ghost">Privacy Policy</Button>
                 <Button kind="danger--ghost">Contact Us</Button>
             </div>
-            <div className="footer-info-holder">
-                <div className="footer-offices-holder">
-                    <h4 className="footer-office-title">Offices</h4>
-                    <div className="footer-office-locations">
-                        <div className="footer-office-hnl">
-                            <h4>Honolulu</h4>
-                            <h6>123 Hnl St</h6>
-                            <h7>12345 Honolulu, Hi</h7>
-                        </div>
-                        <div className="footer-office-london">
-                            <h4>London</h4>
-                            <h6>123 Lnd St</h6>
-                            <h7>12345 London, Hi</h7>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer-social-links">
 
-                </div>
+            <div className="footer-social-links">
+                <IconButton label="Facebook" kind="danger--ghost">
+                    <LogoFacebook />
+                </IconButton>
+                <IconButton label="GitHub" kind="danger--ghost">
+                    <LogoGithub />
+                </IconButton>
+                <IconButton label="Instagram" kind="danger--ghost">
+                    <LogoInstagram />
+                </IconButton>
+                <IconButton label="LinkedIn" kind="danger--ghost">
+                    <LogoLinkedin />
+                </IconButton>
             </div>
         </div>
     )
