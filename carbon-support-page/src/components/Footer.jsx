f27ae307683c57c9e254ed59d793223f9e159c64
murasandev <dmurayama93@gmail.com>
2024-01-25 18:13:37 -1000

    change buttons in footer component to button component from carbon

--- carbon-support-page/src/components/Footer.jsx
+++ carbon-support-page/src/components/Footer.jsx
@@ -1,20 +1,22 @@
+import { Button } from "@carbon/react";
+
 export default function Footer() {
     return(
         <div className="footer-holder">
             {/* careers, about us, contact us, privacy policy,
             offices, social media links */}
             <div className="footer-links-holder">
-                <button className="footer-links">Careers</button>
-                <button className="footer-links">About Us</button>
-                <button className="footer-links">Privacy Policy</button>
-                <button className="footer-links">Contact Us</button>
+                <Button kind="danger--ghost">Careers</Button>
+                <Button kind="danger--ghost">About Us</Button>
+                <Button kind="danger--ghost">Privacy Policy</Button>
+                <Button kind="danger--ghost">Contact Us</Button>
             </div>
             <div className="footer-info-holder">
                 <div className="footer-offices">
 
                 </div>
                 <div className="footer-social-links">
-                    
+
                 </div>
             </div>
         </div>
