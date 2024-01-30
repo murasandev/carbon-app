0be22106d73613b0b892d95a932a70e258591206
murasandev <dmurayama93@gmail.com>
2024-01-29 21:06:32 -1000

    update faq info

--- carbon-support-page/src/components/HelpTopics.jsx
+++ carbon-support-page/src/components/HelpTopics.jsx
@@ -6,21 +6,19 @@ export default function HelpTopics() {
             <div className="help-topics-content-holder">
                 <div className="help-topics-faq">
                     <h1>FAQ </h1>
-                    <Button className='faq-button' kind="danger">Contact Us</Button>
-                    <Button className='faq-button' kind="danger">Contact Us</Button>
-                    <Button className='faq-button' kind="danger">Contact Us</Button>
-                    <Button className='faq-button' kind="danger">Contact Us</Button>
-                    <Button className='faq-button' kind="danger">Contact Us</Button>
-                    <Button className='faq-button' kind="danger">Contact Us</Button>
-                    <Button className='faq-button' kind="danger">Contact Us</Button>
+                    <Button className='faq-button' kind="danger">How do I improve site performance?</Button>
+                    <Button className='faq-button' kind="danger">Are there tools for testing website accessibility?</Button>
+                    <Button className='faq-button' kind="danger">What are the advantages of asynchronous programming?</Button>
+                    <Button className='faq-button' kind="danger">What are the best practices for optimizing a website for all devices?</Button>
+                    <Button className='faq-button' kind="danger">How do I set up a build process for a front-end project?</Button>
                 </div>
                 <div className="help-topics-popular-content">
-                    <h1>Popular Searches </h1>
+                    <h1>Popular Topics </h1>
                     <div className="help-popular-content-holder">
-                        <Button className='help-button' kind="danger">Contact Us</Button>
-                        <Button className='help-button' kind="danger">Contact Us</Button>
-                        <Button className='help-button' kind="danger">Contact Us</Button>
-                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Design Systems</Button>
+                        <Button className='help-button' kind="danger">Git Hub</Button>
+                        <Button className='help-button' kind="danger">Troubleshooting</Button>
+                        <Button className='help-button' kind="danger">Backend Integration</Button>
                     </div>
                     
                     <h1>Front End</h1>
