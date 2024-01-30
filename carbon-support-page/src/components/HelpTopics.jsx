7af6c18c635e8adfd5f2df158e1627c969ce667d
murasandev <dmurayama93@gmail.com>
2024-01-29 19:06:02 -1000

    fix spacing of faq and searches section

--- carbon-support-page/src/components/HelpTopics.jsx
+++ carbon-support-page/src/components/HelpTopics.jsx
@@ -6,6 +6,9 @@ export default function HelpTopics() {
             <div className="help-topics-content-holder">
                 <div className="help-topics-faq">
                     <h1>FAQ </h1>
+                    <div className="faq-button-holder">
+                        
+                    </div>
                     <Button className='faq-button' kind="danger">Contact Us</Button>
                     <Button className='faq-button' kind="danger">Contact Us</Button>
                     <Button className='faq-button' kind="danger">Contact Us</Button>
@@ -16,25 +19,42 @@ export default function HelpTopics() {
                 </div>
                 <div className="help-topics-popular-content">
                     <h1>Popular Searches </h1>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
+                    <div className="help-popular-content-holder">
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                    </div>
+                    
                     <h1>Front End Topics</h1>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
+                    <div className="help-popular-content-holder">
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                    </div>
+
                     <h1>Back End Topics</h1>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
+                    <div className="help-popular-content-holder">
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                    </div>
+
                     <h1>Learning</h1>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
-                    <Button kind="danger">Contact Us</Button>
+                    <div className="help-popular-content-holder">
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                        <Button className='help-button' kind="danger">Contact Us</Button>
+                    </div>
+
+                    
+                    
+                    
+                    
+                    
                 </div>
                 
             </div>
