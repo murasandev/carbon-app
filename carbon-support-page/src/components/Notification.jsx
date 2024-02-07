dcede3286c5ca83b9dc51228478f564c543c8718
murasandev <dmurayama93@gmail.com>
2024-02-06 16:08:16 -1000

    set up state management, and add pictures and titles to switcher

--- carbon-support-page/src/components/Notification.jsx
+++ carbon-support-page/src/components/Notification.jsx
@@ -1,18 +1,22 @@
-import { Button, InlineNotification, Modal } from "@carbon/react";
+import { InlineNotification, Modal, ModalHeader, ModalBody } from "@carbon/react";
+
+
 
 export default function Notification() {
     return(
         <div>
-            
-            <InlineNotification
-                aria-label="closes notification"
-                kind="error"
-                onClose={function noRefCheck(){}}
-                onCloseButtonClick={function noRefCheck(){}}
-                statusIconDescription="notification"
-                subtitle="Status Down"
-                title="Live Chat: "
-              />
+            <Modal danger>
+                <ModalHeader />
+                <InlineNotification
+                    aria-label="closes notification"
+                    kind="error"
+                    onClose={function noRefCheck(){}}
+                    onCloseButtonClick={function noRefCheck(){}}
+                    statusIconDescription="notification"
+                    subtitle="Status Down"
+                    title="Live Chat: "
+                />
+            </Modal>
             
             
             
--- carbon-support-page/src/components/SwitcherImage.jsx
+++ carbon-support-page/src/components/SwitcherImage.jsx
@@ -1,49 +1,53 @@
+import { useState } from 'react';
 import { ContentSwitcher, Switch } from "@carbon/react";
 import ImageHolder from "./ImageHolder";
-import honolulu from '../assets/images/Honolulu.jpg'
-import london from '../assets/images/London.jpeg'
 import './switcherImage.css'
 
-let imgTitle1 = '123';
-let img1;
-
-function CategorySelect(index){
-  switch(index) {
-    case 0:
-      imgTitle1 = 'Honolulu';
-      alert(index);
-      break;
-    case 1:
-      alert(index);
-      imgTitle1 = 'Honolulu2';
-      break;
-    case 2:
-      alert(index);
-      break;
-  }
-}
+import honolulu from '../assets/images/Honolulu.jpg'
+import london from '../assets/images/London.jpeg'
+import frontendImg from '../assets/images/Frontend-Development-Services.png'
+import backendImg from '../assets/images/back-end-developer.jpg'
+import gitHubImg from '../assets/images/GitHub.jpg'
+import stackImg from '../assets/images/stackoverflow.png'
 
 export default function SwitcherImage() {
+  const [img1, setImg1] = useState(honolulu);
+  const [imgTitle1, setImgTitle1] = useState('Honolulu');
+
+  const [img2, setImg2] = useState(london);
+  const [imgTitle2, setImgTitle2] = useState('London');
+
     return(
         <div className="comp-holder">
-          
             <div className="switch-image-holder" >
-                <ImageHolder title={imgTitle1} picture={honolulu} />
-                <ImageHolder title='London' picture={london} />
+                <ImageHolder title={imgTitle1} picture={img1} />
+                <ImageHolder title={imgTitle2} picture={img2} />
             </div>
             <ContentSwitcher 
               onChange={(obj) => {
                 let { index, name, text } = obj;
-                alert(`index: ${index} ||  name: ${name} || text: ${text}`);
+                // alert(`index: ${index} ||  name: ${name} || text: ${text}`);
                 switch(index){
                   case 0:
-                    CategorySelect(index);
+                    setImg1(honolulu);
+                    setImgTitle1('Honolulu');
+                    
+                    setImg2(london);
+                    setImgTitle2('London');
                     break;
                   case 1:
-                    CategorySelect(index);
+                    setImg1(frontendImg);
+                    setImgTitle1('Front End');
+                    
+                    setImg2(backendImg);
+                    setImgTitle2('Back End');
                     break;
                   case 2:
-                    CategorySelect(index);
+                    setImg1(gitHubImg);
+                    setImgTitle1('GitHub');
+                    
+                    setImg2(stackImg);
+                    setImgTitle2('Stack Overflow');
                     break;
                 }
               }}>
